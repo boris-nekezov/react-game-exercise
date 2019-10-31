fdf2d453d3fcbe710f1d2831c470f3365185d3ac
Boris Nekezov <boris.nekezov@gmail.com>
2019-10-31 11:16:15 +0200

    Convert Controls to functional component and fix the text when monster hits

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -1,48 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classes from './Controls.module.css';
 import STATS from '../../utils/STATS';
 
 const { statsDamage, statsBigDamage } = STATS;
 
-class Controls extends Component {
-    render() {
-        const { Button, StartGame, Attack, SpecialAttack, Heal, GiveUp, Controls } = classes;
-        const { start, attacked, healed, gaveUp, gameStarted } = this.props;
-        let gameControls = (
-            <button 
-                className={`${Button} ${StartGame}`}
-                onClick={start}>START NEW GAME</button>
+const Controls = (props) => {
+    const { Button, StartGame, Attack, SpecialAttack, Heal, GiveUp, Controls } = classes;
+    const { start, attacked, healed, gaveUp, gameStarted } = props;
+    let gameControls = (
+        <button 
+            className={`${Button} ${StartGame}`}
+            onClick={start}>START NEW GAME</button>
+    );
+    // if gameStarted is true start game markup
+    if (gameStarted){
+        gameControls = (
+            <div className="small-12 columns">
+                {/* atack */}
+                <button 
+                    className={`${Button} ${Attack}`}
+                    onClick={() => attacked(...statsDamage)}>ATTACK</button> 
+                {/* special attack */}
+                <button 
+                    className={`${Button} ${SpecialAttack}`}
+                    onClick={() => attacked(...statsBigDamage)}>SPECIAL ATTACK</button>
+                {/* heal */}
+                <button 
+                    className={`${Button} ${Heal}`}
+                    onClick={healed}>HEAL</button> 
+                {/* give up */}
+                <button 
+                    className={`${Button} ${GiveUp}`}
+                    onClick={gaveUp}>GIVE UP</button>
+            </div>
         );
-        // if gameStarted is true start game markup
-        if (gameStarted){
-            gameControls = (
-                <div className="small-12 columns">
-                    {/* atack */}
-                    <button 
-                        className={`${Button} ${Attack}`}
-                        onClick={() => attacked(...statsDamage)}>ATTACK</button> 
-                    {/* special attack */}
-                    <button 
-                        className={`${Button} ${SpecialAttack}`}
-                        onClick={() => attacked(...statsBigDamage)}>SPECIAL ATTACK</button>
-                    {/* heal */}
-                    <button 
-                        className={`${Button} ${Heal}`}
-                        onClick={healed}>HEAL</button> 
-                    {/* give up */}
-                    <button 
-                        className={`${Button} ${GiveUp}`}
-                        onClick={gaveUp}>GIVE UP</button>
-                </div>
-            );
-        } 
-
-        return (
-            <section className={`row ${Controls}`}>
-                {gameControls}
-            </section>
-        );
-    }
+    } 
+ 
+    return (
+        <section className={`row ${Controls}`}>
+            {gameControls}
+        </section>
+    );
 }
 
 export default Controls;
