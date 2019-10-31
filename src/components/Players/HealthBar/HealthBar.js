9f40212eef4c33680701f2f8029b689d5f7a0d41
Boris Nekezov <boris.nekezov@gmail.com>
2019-10-31 11:51:44 +0200

    Convert HealthBar component into functional

--- src/components/Players/HealthBar/HealthBar.js
+++ src/components/Players/HealthBar/HealthBar.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classes from './HealthBar.module.css';
 
-class HealthBar extends Component {
-    render() {
-        const { HealthBar } = classes;
-        const { healthPoints } = this.props;
-        return (
-            <div className={HealthBar}>
-                <div
-                    className={`${HealthBar} text-center`}
-                    style={{
-                        backgroundColor: 'green',
-                        margin: '0',
-                        color: 'white',
-                        width: `${healthPoints}%`
-                    }}>
-                    {healthPoints}
-                </div>
+const HealthBar = ({ healthPoints }) => {
+    const { HealthBar } = classes;
+
+    return (
+        <div className={HealthBar}>
+            <div
+                className={`${HealthBar} text-center`}
+                style={{
+                    backgroundColor: 'green',
+                    margin: '0',
+                    color: 'white',
+                    width: `${healthPoints}%`
+                }}>
+                {healthPoints}
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default HealthBar;
